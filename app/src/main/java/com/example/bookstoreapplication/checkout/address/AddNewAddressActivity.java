a2bbcb21203532d7c9d1dc7a0af2159e517384ac
itskriti <kritistha326@gmail.com>
2022-03-24 14:20:45 +0545

    address

---
+++ app/src/main/java/com/example/bookstoreapplication/checkout/address/AddNewAddressActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookstoreapplication.checkout.address;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_address);
+    }
+}
