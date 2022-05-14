48f6fdc62685dc9638216eab12b98ea80fd713fb
itskriti <kritistha326@gmail.com>
2022-05-14 14:08:42 +0545

    author name updated

---
+++ app/src/main/java/com/example/bookstoreapplication/api/response/ProfileResponse.java
@@ -0,0 +1,2 @@
+package com.example.bookstoreapplication.api.response;public class ProfileResponse {
+}
---
+++ app/src/main/java/com/example/bookstoreapplication/orders/orderDetails/OrdersDetailsAdapter.java
@@ -0,0 +1,2 @@
+package com.example.bookstoreapplication.orders.orderDetails;public class ordersDetailsAdapter {
+}
