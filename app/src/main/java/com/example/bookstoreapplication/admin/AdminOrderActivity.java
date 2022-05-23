e4a32d1de5e9e255d3acf6d8a978ad47b95f2b04
itskriti <kritistha326@gmail.com>
2022-05-23 18:40:21 +0545

    order history and profile image upload

---
+++ app/src/main/java/com/example/bookstoreapplication/admin/AdminOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookstoreapplication.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_order);
+    }
+}
